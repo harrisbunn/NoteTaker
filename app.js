cf3acbaaadc518525d245318d862f87bf48231c8
harrisbunn <harrisbunn@gmail.com>
2024-10-21 19:57:48 -0400

    input parsing working

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 import chalk from 'chalk';
 import getNotes from './notes.js';
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
 
-const msg = getNotes;
-console.log(msg);
+let input = yargs(hideBin(process.argv)).parse(); // parse the input arguments , accessed through input._['index'] with 0 being the first argument after npm start
 
-console.log(chalk.blue('Success!'));
+console.log(input._[0]); // access that first argument passed after npm start
--- notes.js
+++ notes.js
@@ -2,4 +2,3 @@ const getNotes = function() {
     return 'Your notes...';
 }
 export default getNotes();
-//module.exports = getNotes;
